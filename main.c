c95be34791b382806b1bb062fec52b9cb4eb940e
Rachel McVicar <c3329436@uon.edu.au>
2019-04-29 01:51:52 +0000

    added work

--- main.c
+++ main.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-  int main()
+void CaesarCipher(char* string, int key); //the function is defined here to be used later in the main code
+
+  int main(void)
   {
-    int i, x;
-    char str[100];
-    printf("\nPlease enter a string:\t");
-    gets(str);
-    printf("\nPlease choose from the following options:\n");
-    printf("1 = Encrypt the string.\n");
-    printf("2 = Decrypt the string.\n");
-    scanf("%d", &x);
+    int key = 1
+    char string[100]; //For this cipher the variables are the key nad the string, thiey are established here
     
-    switch(x)
-    {
-        case 1:
-            for(i = 0; (i < 100) && str[i] != '\0'; i++)
-            str[i] = str[i] + 3;
-            printf("\nEncrypted string: %s\n", str);
-        break;
-        
-        case 2:
-            for(i = 0; (i < 100) && str[i] != '\0'; i++)
-            str[i] = str[i] - 3;
-            printf("\nDecrypted string: %s\n", str);
-        break;
-        
-        default:
-            printf("\nError\n")
-    }
+    printf("\nPlease enter a string:\t");  //Asks th euser to enter the text to be encrypted/decrypted
+    gets(string, sizeof(string), stdin);  //gets the inputed text and stores it in the string variable
+    printf("\nThe ciphered string is:")
+  }
+  void CaesarCipher(char* string, int key);
+  {
+      int i = 0;
+      char cipher;
+      while(string[i] != '\0' && strlen(string) - 1 > i ){  //while i does not equal zero
+      cipher =                      
+          
+          
+      }          
+      
+  }
+
     
   return 0;
   }
